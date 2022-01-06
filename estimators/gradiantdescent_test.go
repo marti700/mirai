ab26a8ba2d9d91abaf2facb49c1ef58317380298
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2022-01-06 15:07:43 -0400

    Corrected gradientdescent test

--- estimators/gradiantdescent_test.go
+++ estimators/gradiantdescent_test.go
@@ -1,23 +1,26 @@
 package estimators_test
 
 import (
-	"fmt"
+	"math"
 	"testing"
 
-	// "github.com/marti700/mirai/estimators"
-	"github.com/marti700/mirai/models/linear"
+	"github.com/marti700/mirai/estimators"
 	"github.com/marti700/veritas/linearalgebra"
 )
 
 func TestGradintDescent(t *testing.T) {
 	//POINTS
 	// (0.5,1.4)
-	// (3.2, 2.9)
-	// (2.4,1.9)
+	// (2.9,3.2)
+	// (2.3,1.9)
+
+	// the gradient used is the one of the MSE function assuming that used on linear regression
+	// extra 1 is added to the data to falcilitate the calculation of the intercept term of the
+	// linear regreassion model
 	data := linearalgebra.NewMatrix([][]float64{
-		{0.5},
-		{2.9},
-		{2.3},
+		{1, 0.5},
+		{1, 2.9},
+		{1, 2.3},
 	})
 
 	target := linearalgebra.NewMatrix([][]float64{
@@ -26,13 +29,35 @@ func TestGradintDescent(t *testing.T) {
 		{1.9},
 	})
 
+	// mean square error function gradient
+	gradient := func(tv linearalgebra.Matrix, f linearalgebra.Matrix, slopes linearalgebra.Matrix) linearalgebra.Matrix {
+		lossFunctionVals := make([]float64, f.Col)
 
-	m := regression.LinearRegression{
-		Hyperparameters: linearalgebra.NewColumnVector([]float64{0,0}),
+		//model predictios with the currently estimated slope values
+		currentModelResults, _ := f.Mult(slopes)
+		for j := 0; j < f.Col; j++ {
+			predErr, _ := tv.Substract(currentModelResults)
+			result, _ := f.GetCol(j).ScaleBy(-2).HadamardProduct(predErr)
+			lossFunctionVals[j] = linearalgebra.ElementsSum(result)
+		}
+		return linearalgebra.NewColumnVector(lossFunctionVals)
 	}
 
-	r := m.Train(target, data, 0.01)
+	gdr := estimators.GradiantDescent(0.01, &data, &target, gradient)
 
-	fmt.Println(r)
-	// estimators.GradiantDescent(1,nil,nil,nil)
+	expected := linearalgebra.NewColumnVector([]float64{0.95, 0.65})
+
+	if !acceptableReslts(expected, *gdr) {
+		t.Error("Error expected result is ", expected, " but was", *gdr)
+	}
+}
+
+//TEST UTILS
+func acceptableReslts(expected, actual linearalgebra.Matrix) bool {
+	for i := range expected.Data {
+		if math.Abs(expected.Data[i]-actual.Data[i]) > 2 {
+			return false
+		}
+	}
+	return true
 }
