be102068f3e18bd45cc911c2c33dcacd430977ee
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2022-01-05 20:03:35 -0400

    Implemented simple working version of the gradient descent algorithm

--- estimators/gradiantdescent.go
+++ estimators/gradiantdescent.go
@@ -1,10 +1,7 @@
 package estimators
 
 import (
-	"errors"
-
 	"github.com/marti700/veritas/linearalgebra"
-	"github.com/marti700/veritas/commons"
 )
 
 // Execute the gradiant descent algorithm
@@ -15,25 +12,26 @@ import (
 func GradiantDescent(learningRate float64,
 	data *linearalgebra.Matrix,
 	target *linearalgebra.Matrix,
-	gradient func(target *linearalgebra.Matrix,
-		data *linearalgebra.Matrix,
-		slopes linearalgebra.Matrix) linearalgebra.Matrix) (*linearalgebra.Matrix, error) {
+	gradient func(target linearalgebra.Matrix,
+		data linearalgebra.Matrix,
+		slopes linearalgebra.Matrix) linearalgebra.Matrix) *linearalgebra.Matrix {
 
-	if data.Col != target.Row {
-		return nil, errors.New("data matrix features should be equal to target size")
-	}
-
-	// generate slopes initial values (I guess they can be zero) -
+	// generate slopes initial values (I guess they can be zero ...) -
 	slopesVal := linearalgebra.NewColumnVector(make([]float64, data.Col))
 	tempSlopeVals := linearalgebra.NewColumnVector(make([]float64, data.Col))
 
+	// TODO: Implement gradiant descent huristics
+	// Max number of iteration before giving up
+	// Stop when the gradiant stop making significant advances towards the bottom of the hill
 	for i := 0; i < 1000; i++ {
-		for j := 0; j < data.Col; j++ {
-			tempSlopeVals.Data[j] = tempSlopeVals.Data[j] - (learningRate * commons.Sum(gradient(target, data, slopesVal).Data))
-		}
-		// after eatch iteration simultaniuosly update the slopes
+		// to determine the slopes values we do:
+		// 1- Solves for the slop using the target and data parameters
+		// 2- multiplies for the learning rate
+		// 3- substract to old step size from the result obtained in the previous operationc
+		tempSlopeVals, _ = tempSlopeVals.Substract(gradient(*target, *data, slopesVal).ScaleBy(learningRate))
+		// simultaniuosly update the slopes to their new values
 		copy(slopesVal.Data, tempSlopeVals.Data)
 	}
 	newVectorsVal := slopesVal
-	return &newVectorsVal, nil
+	return &newVectorsVal
 }
--- estimators/gradiantdescent_test.go
+++ estimators/gradiantdescent_test.go
@@ -1,10 +1,38 @@
 package estimators_test
 
 import (
-	"github.com/marti700/mirai/estimators"
+	"fmt"
 	"testing"
+
+	// "github.com/marti700/mirai/estimators"
+	"github.com/marti700/mirai/models/linear"
+	"github.com/marti700/veritas/linearalgebra"
 )
 
 func TestGradintDescent(t *testing.T) {
-	estimators.GradiantDescent(1,nil,nil,nil)
+	//POINTS
+	// (0.5,1.4)
+	// (3.2, 2.9)
+	// (2.4,1.9)
+	data := linearalgebra.NewMatrix([][]float64{
+		{0.5},
+		{2.9},
+		{2.3},
+	})
+
+	target := linearalgebra.NewMatrix([][]float64{
+		{1.4},
+		{3.2},
+		{1.9},
+	})
+
+
+	m := regression.LinearRegression{
+		Hyperparameters: linearalgebra.NewColumnVector([]float64{0,0}),
+	}
+
+	r := m.Train(target, data, 0.01)
+
+	fmt.Println(r)
+	// estimators.GradiantDescent(1,nil,nil,nil)
 }
