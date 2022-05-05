3cc603720238ebcecb1f0f43b63eeda244b560bf
Teodoro Alcántara Aquino <teodoro641@hotmail.com>
2022-05-05 07:03:18 -0400

    Added acc function to calculate classification accuarcy

---
+++ metrics/metrics.go
@@ -0,0 +1,30 @@
+package metrics
+
+import (
+	"github.com/marti700/veritas/linearalgebra"
+)
+
+// calculates the mean square error of two column vectors
+func MeanSquareError(actual, predicted linearalgebra.Matrix) float64 {
+	dataPoints := actual.Row
+	squareF := func(x float64) float64 { return x * x }
+	return linearalgebra.ElementsSum(actual.Substract(predicted).Map(squareF)) / float64(dataPoints)
+}
+
+// calculate the accuarcy of classification
+// the predicted argument is the data used for testing the model, the one we want to classify
+// the actual argument are the real classes of the dataset
+// both predicted and actual must be column vectors of the same dimensions, if they are not this function will panic
+func acc(predicted, actual linearalgebra.Matrix) float64 {
+	// TODO: Panic if predicted or actual are not vectors
+	// TODO: Panic if predicted and actual are not of the same size
+	var diff int
+	tElements := len(predicted.Data)
+	for i := range predicted.Data {
+		if predicted.Data[i] != actual.Data[i] {
+			diff++
+		}
+	}
+
+	return (float64(tElements - diff)) / float64(tElements)
+}
---
+++ metrics/metrics_test.go
@@ -0,0 +1,39 @@
+package metrics
+
+import (
+	"testing"
+	"github.com/marti700/veritas/linearalgebra"
+)
+
+func TestMeanSquareError(t *testing.T) {
+	actual := linearalgebra.NewColumnVector(   []float64 {1,2,3,4,5,6,7,8,9,10})
+	predicted := linearalgebra.NewColumnVector([]float64 {10,9,8,7,6,5,4,3,2,1})
+	expected := 33.0
+	result :=  MeanSquareError(actual,predicted)
+
+	if result != 33.0 {
+		t.Error("Expected result is: ",expected, "but was :", result )
+	}
+
+}
+
+func TestAcc(t *testing.T) {
+	predicted1 := linearalgebra.NewColumnVector([]float64 {0,0,1,1,0,1,0,1,0,0,1,1,1,0,0,1,1,1,1,0,1,0,1,1,0,1,1,0,0,1})
+	predicted2 := linearalgebra.NewColumnVector([]float64 {0,0,1,1,0,1,0,0,0,0,1,1,1,0,0,1,1,1,1,0,1,0,1,1,0,1,1,0,0,0})
+	actual := linearalgebra.NewColumnVector([]float64{0,0,1,1,1,1,0,0,0,0,1,1,1,0,0,1,1,1,1,0,1,0,0,1,0,1,1,0,1,0})
+
+	expected1 := 0.8333333333333334
+	expected2 := 0.90
+
+	acc1 := acc(predicted1, actual)
+	acc2 := acc(predicted2, actual)
+
+	if expected1 != acc1 {
+		t.Error("Expected result is: ",expected1, "but was :", acc1 )
+	}
+
+if expected2 != acc2 {
+		t.Error("Expected result is: ",expected2, "but was :", acc2 )
+	}
+
+}
