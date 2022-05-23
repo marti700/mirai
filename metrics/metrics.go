2c41179e66ebc847e4468ab4f75f14588448bdb2
Teodoro Alcántara Aquino <teodoro641@hotmail.com>
2022-05-23 07:55:18 -0400

    Refactor MeanSquareError function

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -7,8 +7,7 @@ import (
 // calculates the mean square error of two column vectors
 func MeanSquareError(actual, predicted linearalgebra.Matrix) float64 {
 	dataPoints := actual.Row
-	squareF := func(x float64) float64 { return x * x }
-	return linearalgebra.ElementsSum(actual.Substract(predicted).Map(squareF)) / float64(dataPoints)
+	return RSS(actual,predicted) / float64(dataPoints)
 }
 
 // calculate the accuarcy of classification
